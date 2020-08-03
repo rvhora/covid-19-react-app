6387c3d62196de8cbd99f8ceb068096bb003b568
rvhora <rupeshvhora1988@gmail.com>
2020-08-03 12:20:11 +1000

    added build file and configurations CNAMe file

---
+++ build/precache-manifest.e32d9b0bd94d08f71569eccf977b8c3c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc2c14db9e612db5c6142e23e0d087ec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fe12c5a40fa3618a57b2",
+    "url": "/static/css/main.bc868559.chunk.css"
+  },
+  {
+    "revision": "0596ef68d3d012f4469f",
+    "url": "/static/js/2.05fb249c.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/static/js/2.05fb249c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe12c5a40fa3618a57b2",
+    "url": "/static/js/main.bd8467a2.chunk.js"
+  },
+  {
+    "revision": "fc67fad158ce2762ef25",
+    "url": "/static/js/runtime-main.02b54de1.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/static/media/image.d7265326.png"
+  }
+]);
